c1ccd4fe789f229b0c365b132ac0e04b1fc71049
lastnitescurry <Brian.Dinneen@gmail.com>
2015-11-25 21:48:14 +0000

    DA user properties fix

--- puppet/modules/applicationserver/manifests/init.pp
+++ puppet/modules/applicationserver/manifests/init.pp
@@ -3,17 +3,28 @@
 # Deploy documentum applications on a provisioned tomcat instance
 #
 class applicationserver {
-
+  $catalina_home = '/u01/app/apache-tomcat/apache-tomcat-7.0.65'
+  
   class { 'tomcat':
-    catalina_home       => '/u01/app/apache-tomcat/documentum',
-    manage_user         => false,
-    manage_group        => false,
-  }->
+    catalina_home  => $catalina_home,
+    manage_user    => false,
+    manage_group   => false,
+  }~>
   tomcat::instance { 'apache-tomcat-7.0.65':
-    catalina_base       => '/u01/app/apache-tomcat/apache-tomcat-7.0.65',
-    catalina_home       => '/u01/app/apache-tomcat/documentum',
-    source_url          => '/software/Apache/Tomcat/apache-tomcat-7.0.65.tar.gz'
+    catalina_base  => $catalina_home,
+    catalina_home  => $catalina_home,
+    source_url     => '/software/Apache/Tomcat/apache-tomcat-7.0.65.tar.gz',
   }
+
+  file { 'catalina.properties':
+    path    => '/u01/app/apache-tomcat/apache-tomcat-7.0.65/conf/catalina.properties',
+    source  => 'puppet:///modules/applicationserver/catalina.properties',
+    owner   => 'tomcat',
+    group   => 'tomcat',
+    mode    => '0644',
+    require => Tomcat::Instance['apache-tomcat-7.0.65'],
+  }
+
 #  }->
 #  tomcat::service { 'default':
 #    catalina_base       => '/u01/app/apache-tomcat/apache-tomcat-7.0.65',
