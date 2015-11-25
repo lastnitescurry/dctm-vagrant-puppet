46075687f2c8558865bc27030e784e39609ff1de
lastnitescurry <Brian.Dinneen@gmail.com>
2015-11-25 21:25:46 +0000

    Move Tomcat Install and configuration to module. Add out of the box tomcat configuration file.

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -37,21 +37,5 @@ node 'csnode' {
 
 }
 node 'appsservr' {
-  class { 'tomcat':
-    catalina_home       => '/u01/app/apache-tomcat/documentum',
-    manage_user         => false,
-    manage_group        => false,
-  }->
-  tomcat::instance { 'apache-tomcat-7.0.65':
-    catalina_base       => '/u01/app/apache-tomcat/apache-tomcat-7.0.65',
-    catalina_home       => '/u01/app/apache-tomcat/documentum',
-    source_url          => '/software/Apache/Tomcat/apache-tomcat-7.0.65.tar.gz'
-  }->
-  tomcat::service { 'default':
-    catalina_base       => '/u01/app/apache-tomcat/apache-tomcat-7.0.65',
-  }->
-  tomcat::war { 'da.war':
-    catalina_base       => '/u01/app/apache-tomcat/apache-tomcat-7.0.65',
-    war_source          => '/software/Documentum/D71/da.war',
-  }
+  include applicationserver
 }
